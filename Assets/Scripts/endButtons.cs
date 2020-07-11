ccf0f482898077a9baf21197fd7aa4d8e0a7cfeb
bigassopper <61640594+bigassopper@users.noreply.github.com>
2020-07-11 17:29:27 -0300

    level cleared

---
+++ Assets/Scripts/endButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endButtons : MonoBehaviour
+{
+    public int nextRoom;
+    public int actualRoom;
+    public GameObject endUI;
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(actualRoom);
+        Time.timeScale = 1f;
+        endUI.SetActive(false);
+        gameStates.restarted = true;
+    }
+
+    public void Next()
+    {
+        SceneManager.LoadScene(nextRoom);
+        Time.timeScale = 1f;
+        gameStates.restarted = true;
+    }
+}
---
+++ Assets/Scripts/newLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newLevel : MonoBehaviour
+{
+    public GameObject endUI;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        Time.timeScale = 0f;
+        endUI.SetActive(true);
+    }
+
+}
